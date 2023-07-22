d984bbdad275bfe3565cd0855dd3adf3e3fee233
VandeJr <vanderlei.jordao.jr@outlook.com>
2023-07-21 23:25:43 -0300

    Adding anime sql representation

---
+++ Anime.sql
@@ -0,0 +1,12 @@
+CREATE TABLE anime (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    english_title VARCHAR(100) NOT NULL,
+    jap_title VARCHAR(100) NOT NULL,
+    synopsis TEXT,
+    studio VARCHAR(100),
+    director VARCHAR(100),
+    total_eps INT,
+    release_date DATE,
+    rating VARCHAR(5),
+    mean_score DECIMAL(3, 2)
+);
